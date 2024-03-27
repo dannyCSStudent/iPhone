797f766d27978c1e439da68b084264883590a030
dannyCSStudent <danv8605@gmail.com>
2024-03-27 06:03:44 -0600

    try again

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 import * as Sentry from '@sentry/react';
 
 const App = () => {
-  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+  
   return (
     <main className="bg-black">
       <Navbar />
