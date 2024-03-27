909bf911372d58186e3978398669371cbd875caa
dannyCSStudent <danv8605@gmail.com>
2024-03-27 05:58:53 -0600

    changed ModelView and then deployed again

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
 import * as THREE from 'three'
 import Lights from './Lights';
